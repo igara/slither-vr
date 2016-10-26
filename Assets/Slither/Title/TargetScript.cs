ed2a513a81ee5ed4117c5ed9bb577f1a2af6d044
五十嵐翔 <igara1119@gmail.com>
2016-10-27 01:23:41 +0900

    タイトルから次の画面へ遷移するように

---
+++ Assets/Slither/Title/TargetScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Target script.
+/// </summary>
+public class TargetScript : MonoBehaviour {
+
+	private float timeleft;
+	private int time = 6;
+
+	/// <summary>
+	/// Raises the trigger enter event.
+	/// </summary>
+	/// <param name="collider">Collider.</param>
+	void OnTriggerEnter(Collider collider) {
+	}
+
+	/// <summary>
+	/// Raises the trigger stay event.
+	/// </summary>
+	/// <param name="collider">Collider.</param>
+	void OnTriggerStay(Collider collider) {
+		if (collider.gameObject.tag == "Start") {
+			TextMesh start = collider.gameObject.GetComponent<TextMesh>();
+			//だいたい1秒ごとに処理を行う
+			timeleft -= Time.deltaTime;
+			if (timeleft <= 0.0) {
+				timeleft = 1.0f;
+				time -= 1;
+				start.text = time.ToString();
+				if (time == 0) {
+					SceneManager.LoadScene("Slither/Doom/DoomScene");
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// Raises the trigger exit event.
+	/// </summary>
+	/// <param name="collider">Collider.</param>
+	void OnTriggerExit(Collider collider){
+		if (collider.gameObject.tag == "Start") {
+			TextMesh start = collider.gameObject.GetComponent<TextMesh>();
+			start.text = "Start";
+			time = 6;
+		}
+	}
+}
---
+++ Assets/Slither/Title/TitleSceneScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Title scene script.
+/// </summary>
+public class TitleSceneScript : MonoBehaviour {
+
+	/// <summary>
+	/// The m camera.
+	/// </summary>
+	[SerializeField] GameObject m_camera;
+
+	/// <summary>
+	/// The m title.
+	/// </summary>
+	[SerializeField] GameObject m_title;
+
+	/// <summary>
+	/// The m start.
+	/// </summary>
+	[SerializeField] GameObject m_start;
+
+	/// <summary>
+	/// The m target mark.赤い×印
+	/// </summary>
+	[SerializeField] GameObject m_target_mark;
+
+	/// <summary>
+	/// The m target dummy.
+	/// </summary>
+	[SerializeField] GameObject m_target_dummy;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start () {
+
+	}
+	
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
+	void Update () {
+
+	}
+}
