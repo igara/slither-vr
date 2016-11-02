dcf79cb6fb012d1175016b42a71a50ef55866db2
五十嵐翔 <igara1119@gmail.com>
2016-11-02 23:42:05 +0900

    カメラが前進する対応

--- Assets/Slither/Doom/DoomSceneScript.cs
+++ Assets/Slither/Doom/DoomSceneScript.cs
@@ -21,20 +21,37 @@ public class DoomSceneScript : MonoBehaviour {
 	/// The m target dummy.
 	/// </summary>
 	[SerializeField] GameObject m_target_dummy;
+	public float moveSpeed  = 10.0f;
+
+	float yOffset;
 
 	/// <summary>
 	/// Start this instance.
 	/// </summary>
 	void Start () {
-		offset = m_camera.transform.position - m_target_mark.transform.position;
+		yOffset = m_camera.transform.position.y;
+
 
 	}
-	private Vector3 offset = Vector3.zero;
 
 	/// <summary>
 	/// Update this instance.
 	/// </summary>
 	void Update () {
-		
+		// 1.カメラの傾きを取得
+		float x = m_camera.transform.eulerAngles.x;
+		Debug.Log (x);
+
+		// 2.ある角度以内であれば前進させる
+		//-20
+		if ((0.0f <= x && x <= 30.0f) || (330.0f <= x && x <= 360.0f)) {
+			moveFoward ();
+		}
+	}
+	private void moveFoward() {
+		Vector3 direction = new Vector3 (m_camera.transform.forward.x, 0, m_camera.transform.forward.z).normalized * moveSpeed * Time.deltaTime;
+		Quaternion rotation = Quaternion.Euler (new Vector3 (0, -m_camera.transform.rotation.eulerAngles.y, 0));
+		m_camera.transform.Translate (rotation * direction);
+		m_camera.transform.position = new Vector3 (m_camera.transform.position.x, yOffset, m_camera.transform.position.z);
 	}
 }
