e08ae4ebe9639d027e56b34104b7517aaadd9797
五十嵐翔 <igara1119@gmail.com>
2016-11-02 23:04:57 +0900

    床のモデルと芋虫のモデルの追加

---
+++ Assets/Slither/Doom/DoomSceneScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Doom scene script.
+/// </summary>
+public class DoomSceneScript : MonoBehaviour {
+
+	/// <summary>
+	/// The m camera.
+	/// </summary>
+	[SerializeField] GameObject m_camera;
+
+	/// <summary>
+	/// The m target mark.赤い×印
+	/// </summary>
+	[SerializeField] GameObject m_target_mark;
+
+	/// <summary>
+	/// The m target dummy.
+	/// </summary>
+	[SerializeField] GameObject m_target_dummy;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start () {
+		offset = m_camera.transform.position - m_target_mark.transform.position;
+
+	}
+	private Vector3 offset = Vector3.zero;
+
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
+	void Update () {
+		
+	}
+}
