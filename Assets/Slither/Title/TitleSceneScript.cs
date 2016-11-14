ca336da513a2002d90ae6c4b4dedc144dd90f84e
五十嵐翔 <igara1119@gmail.com>
2016-11-14 21:44:13 +0900

    子の衝突を親で検知できる様になったため不要なスクリプトの削除

--- Assets/Slither/Title/TitleSceneScript.cs
+++ Assets/Slither/Title/TitleSceneScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Title scene script.
@@ -33,6 +34,15 @@ public class TitleSceneScript : MonoBehaviour {
 	[SerializeField] GameObject m_target_dummy;
 
 	/// <summary>
+	/// The timeleft.
+	/// </summary>
+	private float timeleft;
+	/// <summary>
+	/// The time.
+	/// </summary>
+	private int time = 6;
+
+	/// <summary>
 	/// Start this instance.
 	/// </summary>
 	void Start () {
@@ -44,5 +54,49 @@ public class TitleSceneScript : MonoBehaviour {
 	/// </summary>
 	void Update () {
 
+	}
+
+	/// <summary>
+	/// Redirecteds the on trigger enter. 子のゲームオブジェクトのcolliderを開始検知する
+	/// </summary>
+	/// <param name="collider">Collider.</param>
+	public void RedirectedOnTriggerEnter (Collider collider)
+	{
+		string gameobject_name = collider.gameObject.name;
+
+	}
+
+	/// <summary>
+	/// Redirecteds the on trigger exit.
+	/// </summary>
+	/// <param name="collider">Collider.</param>
+	public void RedirectedOnTriggerExit (Collider collider)
+	{
+		if (collider.gameObject.tag == "SinglePlay") {
+			TextMesh start = collider.gameObject.GetComponent<TextMesh>();
+			start.text = "SinglePlay";
+			time = 6;
+		}
+	}
+
+	/// <summary>
+	/// Redirecteds the on trigger stay. 子のゲームオブジェクトのcolliderをキープ検知する
+	/// </summary>
+	/// <param name="collider">Collider.</param>
+	public void RedirectedOnTriggerStay (Collider collider)
+	{
+		if (collider.gameObject.tag == "SinglePlay") {
+			TextMesh start = collider.gameObject.GetComponent<TextMesh>();
+			//だいたい1秒ごとに処理を行う
+			timeleft -= Time.deltaTime;
+			if (timeleft <= 0.0) {
+				timeleft = 1.0f;
+				time -= 1;
+				start.text = time.ToString();
+				if (time == 0) {
+					SceneManager.LoadScene("Slither/SingleDoom/SingleDoomScene");
+				}
+			}
+		}
 	}
 }
